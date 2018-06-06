65e98aef06fb7a07f1dce0feb093d9d392ad052b
Prajeesh <prajeeshag@gmail.com>
2018-06-06 13:48:19 +0530

    2dc2c working

--- fftw3.F90
+++ fftw3.F90
@@ -1,29 +1,40 @@
 module FFTW3
-
+    
     use, intrinsic :: iso_c_binding
 
     use fms_mod, only : open_namelist_file, close_file, error_mesg, FATAL, WARNING, NOTE
 
     include 'fftw3-mpi.f03'
 
-    type(C_PTR) :: plan3d, c_cout, c_rin
-    real(C_DOUBLE), pointer :: rin(:,:)
-    complex(C_DOUBLE_COMPLEX), pointer :: cout(:,:)
-    integer(C_INTPTR_T), parameter :: rank=2, M=2, M2=M*2
+    private
 
-    integer :: howmany3d
+    integer(C_INTPTR_T), parameter :: M=2, M2=M*2
+    integer :: rank=2
+    integer(C_INTPTR_T) :: howmany3d, nlat, nlev
+
+    type(C_PTR) :: plan3d, c_cout, c_rin
+    real(C_DOUBLE), pointer :: rin(:,:,:)
+    complex(C_DOUBLE_COMPLEX), pointer :: cout(:,:,:)
 
     logical :: initialized = .false.
     integer :: plan_level=-1
+    character(len=8) :: method='2dc2c'
 
-    namelist/fftw3_nml/plan_level
+    public :: init_fftw3, fft, method, fft_1dr2c_serial, fft_1dc2c_serial
+
+    interface fft
+        module procedure fft_2dr2c
+        module procedure fft_2dc2c
+    end interface fft
+
+    namelist/fftw3_nml/plan_level, method
 
     contains
 
-    subroutine init_fftw3 (n0_in, nlat, nlev, comm, ni_local, i_start_local, no_local, o_start_local)
+    subroutine init_fftw3 (n0_in, nlat_in, nlev_in, comm, ni_local, i_start_local, no_local, o_start_local)
 
         implicit none
-        integer, intent(in) :: n0_in, nlat, nlev, comm
+        integer, intent(in) :: n0_in, nlat_in, nlev_in, comm
         integer, intent(out) :: ni_local, i_start_local, no_local, o_start_local
         integer(C_INTPTR_T) :: n0, iblock, oblock, n(rank)
         integer(C_INTPTR_T) :: i, j, alloc_local, local_ni, local_i_start, local_no, local_o_start
@@ -53,26 +64,46 @@ module FFTW3
 
         n(1) = n0_in
         n(2) = M
+        n0 = n0_in
+
+        nlat = nlat_in
+        nlev = nlev_in
 
         howmany3d = nlat*nlev
         
         call fftw_mpi_init()
 
-        alloc_local = fftw_mpi_local_size_many(rank, n, howmany3d, iblock, comm, &
-                                                        local_ni, local_i_start)
-        local_no = local_ni
-        local_o_start = local_i_start
+        if (trim(method)=='2dr2c') then
+            rank = 2
+            alloc_local = fftw_mpi_local_size_many(rank, n, howmany3d, iblock, comm, &
+                                                            local_ni, local_i_start)
+            local_no = local_ni
+            local_o_start = local_i_start
 
-        c_rin = fftw_alloc_real(2*alloc_local)
-        c_cout = fftw_alloc_complex(alloc_local)
+            c_rin = fftw_alloc_real(2*alloc_local)
+            c_cout = fftw_alloc_complex(alloc_local)
 
-        print *, 'alloc_local=', alloc_local
+            call c_f_pointer(c_rin, rin, [nlat, nlev, M2, local_ni])
+            call c_f_pointer(c_cout, cout, [howmany3d, M, local_no])
 
-        call c_f_pointer(c_rin, rin, [M2, local_ni])
-        call c_f_pointer(c_cout, cout, [M, local_no])
+            plan3d = fftw_mpi_plan_many_dft_r2c (rank, n, howmany3d, iblock, oblock, &
+                                               rin, cout, comm, flags)
 
-        plan3d = fftw_mpi_plan_many_dft_r2c (rank, n, howmany, iblock, oblock, &
-                                           rin, cout, comm, flags)
+        elseif (trim(method)=='2dc2c') then
+
+            rank = 2
+            alloc_local = fftw_mpi_local_size_many(rank, n, howmany3d, iblock, comm, &
+                                                            local_ni, local_i_start)
+            local_no = local_ni
+            local_o_start = local_i_start
+
+            c_cout = fftw_alloc_complex(alloc_local)
+
+            call c_f_pointer(c_cout, cout, [howmany3d, M, local_no])
+            
+            plan3d = fftw_mpi_plan_many_dft(rank, n, howmany3d, iblock, oblock, & 
+                                            cout, cout, comm, FFTW_FORWARD, flags)
+        endif
 
         ni_local = local_ni
         i_start_local = local_i_start
@@ -81,13 +112,79 @@ module FFTW3
         
         initialized = .true.
 
-        call error_mesg(routine, 'FFTW3 initialized', NOTE)
+        call error_mesg(routine, 'FFTW3 initialized with method :'//trim(method), NOTE)
 
     end subroutine init_fftw3
 
+    subroutine fft_2dr2c(rinp, coutp)
 
-   ! subroutine fft_3d(rinp,coutp)
+        real, dimension (:, :, :) :: rinp
+        complex, dimension (:, :, :) :: coutp
+        
+        if (trim(method)/='2dr2c') call error_mesg('fft_2dr2c', 'Method is not 2dr2c', FATAL) 
 
-   !     real, dimension(2,:) ::  
-   !      
+        rin(:,1:M,:) = rinp(:,1:M,:)
+
+        call fftw_mpi_execute_dft_r2c(plan3d, rin, cout) 
+
+        coutp(:,1:M,:) = cout(:,1:M,:)
+
+    end subroutine fft_2dr2c
+
+    subroutine fft_2dc2c(coutp)
+        complex, dimension (:, :, :) :: coutp
+        
+        if (trim(method)/='2dc2c') call error_mesg('fft_2dc2c', 'Method is not 2dc2c', FATAL) 
+
+        cout(:,1:M,:) = coutp(:,1:M,:)
+
+        call fftw_mpi_execute_dft(plan3d, cout, cout) 
+
+        coutp(:,1:M,:) = cout(:,1:M,:)
+    end subroutine fft_2dc2c
+
+    subroutine fft_1dr2c_serial(rinp, coutp)
+
+        real :: rinp(:)
+        complex :: coutp(:)
+        real(C_DOUBLE), pointer :: rinpl(:)
+        complex(C_DOUBLE_COMPLEX), pointer :: coutpl(:)
+        type(C_PTR) :: plan1d, data1d
+        integer :: L
+        L = size(rinp)
+
+        data1d = fftw_alloc_complex(int(L/2+1, C_SIZE_T))
+        call c_f_pointer(data1d, rinpl, [2*(L/2+1)])
+        call c_f_pointer(data1d, coutpl, [L/2+1])
+   
+        plan1d = fftw_plan_dft_r2c_1d(L, rinpl, coutpl, FFTW_ESTIMATE) 
+
+        rinpl = rinp
+        call fftw_execute_dft_r2c(plan1d, rinpl, coutpl)
+        coutp = coutpl
+        
+        call fftw_destroy_plan(plan1d)
+        call fftw_free(data1d)
+    end subroutine fft_1dr2c_serial
+
+    subroutine fft_1dc2c_serial(coutp)
+        complex :: coutp(:)
+        complex(C_DOUBLE_COMPLEX), pointer :: coutpl(:)
+        type(C_PTR) :: plan1d, data1d
+        integer :: L
+        L = size(coutp)
+
+        data1d = fftw_alloc_complex(int(L, C_SIZE_T))
+        call c_f_pointer(data1d, coutpl, [L])
+   
+        plan1d = fftw_plan_dft_1d(L, coutpl, coutpl, FFTW_FORWARD, FFTW_ESTIMATE) 
+
+        coutpl = coutp
+        call fftw_execute_dft(plan1d, coutpl, coutpl)
+        coutp = coutpl
+        
+        call fftw_destroy_plan(plan1d)
+        call fftw_free(data1d)
+    end subroutine fft_1dc2c_serial
+ 
 end module
--- grid_to_fourier.F90
+++ grid_to_fourier.F90
@@ -1,13 +1,16 @@
 module grid_to_fourier_mod
 
-    use FFTW3
+    use FFTW3, only : init_fftw3, fft, method, fft_1dr2c_serial, fft_1dc2c_serial
 
     use mpp_mod, only : mpp_init, FATAL, WARNING, NOTE, mpp_error
     use mpp_mod, only : mpp_npes, mpp_get_current_pelist, mpp_pe
-    use mpp_mod, only : mpp_exit
- 
+    use mpp_mod, only : mpp_exit, mpp_clock_id, mpp_clock_begin, mpp_clock_end
+    use mpp_mod, only : mpp_sync
+    use fms_mod, only : read_data, write_data, open_namelist_file, close_file
+    use fms_io_mod, only : fms_io_exit 
+
     implicit none
-    
+    integer, parameter :: sp = kind(1e0), dp = kind(1d0)    
      
     contains
 
@@ -21,25 +24,194 @@ module grid_to_fourier_mod
         integer, allocatable :: pelist(:) 
  
         character(len=32) :: routine='init_grid_to_fourier'
-    
+
         integer :: comm
- 
+
+        real, allocatable :: fld(:,:,:), fld1d(:), fld1dout(:)
+        complex, allocatable :: fldc1d(:), fldc(:,:,:), fldc1(:,:,:)
+        real :: AUX1CRS(42002)
+        complex, allocatable :: four_fld(:,:,:)
+        integer :: isc, iec, isg, ieg, m, l, t, i, ig, k, kstart=0, kend=0, kstep=1
+        real :: scl, x, y
+        integer :: clck_fftw3, clck_drcft, init, unit
+        complex :: wgt, a, b, c, d, e
+        complex(kind=4) :: cpout(3)
+        logical :: oddeven=.true.
+        real, parameter :: PI=4.D0*DATAN(1.D0)
+        complex, parameter :: ui = cmplx(0.,1.), mui = -1.*ui
+        complex, parameter :: cone = cmplx(1.,0.), w1b2=exp(mui*2.*pi*0.5)
+        complex, parameter :: one_w1b2 = cone - w1b2
+        complex :: wlbN
+
+        namelist/grid_to_fourier_nml/kstart, kend, kstep, oddeven
+
         call mpp_init() 
-       
+ 
         if (mod(nlon,2)/=0) call mpp_error(routine, 'nlon is not an ever number', FATAL)
         
         nlonb2 = nlon/2
+
+        if (mod(nlonb2,mpp_npes())/=0)  &
+         call mpp_error(routine, 'No: procs in x-dir is not a factor of nlon/2', FATAL)
+
+        unit = open_namelist_file()
+        read(unit,nml=grid_to_fourier_nml)
+        call close_file(unit)
+
+        clck_fftw3 = mpp_clock_id('fftw3')
 
         allocate(pelist(mpp_npes()))
 
         call mpp_get_current_pelist(pelist,commid=comm)
         
         call init_fftw3 (nlonb2, nlat, nlev, comm, ilen, istart, olen, ostart)
-            
-        print *, mpp_pe(), ilen, istart, olen, ostart
+        
+        isc = istart + 1; iec = isc + ilen - 1
 
-        call mpp_exit()
+        allocate(fld(nlat*nlev,2,isc:iec))
+        allocate(fldc(nlat*nlev,2,isc:iec))
+        allocate(fldc1(nlat*nlev,2,isc:iec))
+        fld = 0.
+        allocate(four_fld(nlat*nlev,2,isc:iec))
+        allocate(fld1d(nlon))
+        allocate(fld1dout(nlon))
+            allocate(fldc1d(nlon))
+        
+        !call read_data('test_data.nc', 'tas', fld1d)
+
+        scl=1./nlon
+
+        fld1d=0.
+        do k = kstart, kend, kstep
+            do i = 0, nlon-1
+                fld1d(i+1) = fld1d(i+1) + 2*k*cos(2.*PI*i*k/nlon)
+                fldc1d(i+1) = cmplx(fld1d(i+1),0.)*scl
+            enddo
+        enddo
+
+
+        do l = 1, nlev*nlat
+            do m = 1, 2
+                if (oddeven) then
+                    do i = isc, iec
+                        ig = 2*(i-1)+m
+                        print *, m, i, ig
+                        fld(l,m,i) = fld1d(ig)*scl
+                        fldc1(l,m,i) = cmplx(fld1d(ig),0.)*scl
+                    enddo
+                else
+                    isg = (m-1)*nlonb2+isc
+                    ieg = (m-1)*nlonb2+iec
+                    fld(l,m,isc:iec) = fld1d(isg:ieg)*scl
+                endif
+            end do
+        end do
+      
+        call mpp_sync()
  
+        call mpp_clock_begin(clck_fftw3)
+        if (trim(method)=='2dr2c') then
+            call fft(fld,four_fld)
+            call write_data('fftw_out.nc','real1', real(four_fld(1,:,:)))
+            call write_data('fftw_out.nc','imag1', dimag(four_fld(1,:,:)))
+            call write_data('fftw_out.nc','mag1', abs(four_fld(1,:,:)))
+        endif
+        call mpp_clock_end(clck_fftw3)
+        
+        print *, 'testing 2D method'
+        print *, '' 
+        print *, 'X1...'
+        fldc = fldc1
+        do m = 1, size(fldc,2)
+            call fft_1dc2c_serial(fldc(1,m,:))
+        enddo
+
+        do i = 0, nlon/2-1
+            cpout(1) = (fldc(1,1,i+1)) !* exp(-1.*ui*2.*pi*real(i)/nlon) 
+            cpout(2) = (fldc(1,2,i+1)) !* exp(-1.*ui*2.*pi*real(i)/nlon)
+            print *, i, cpout(1:2)
+        enddo
+
+        print *, '' 
+        print *, 'X2...'
+        print *, 'ui=', ui
+        do l = 1, size(fldc,3)
+            print *, 'wgt=', wgt
+            do i = 1, size(fldc,2)
+                wgt = exp((-1.*ui*2.*PI/nlon)*(l-1)*(i-1))
+                fldc(1,i,l) = fldc(1,i,l) * wgt
+            enddo
+            call fft_1dc2c_serial(fldc(1,:,l))
+        enddo
+
+        do i = 0, nlon/2
+            cpout(1) = (fldc(1,1,i+1)) !* exp(-1.*ui*2.*pi*real(i)/nlon) 
+            cpout(2) = (fldc(1,2,i+1)) !* exp(-1.*ui*2.*pi*real(i)/nlon)
+            print *, i, cpout(1:2)
+        enddo
+
+        print *, 'end testing 2d method'
+        print *, '' 
+
+        if (trim(method)=='2dc2c') then
+            fldc = fldc1
+            call fft(fldc)
+            !call write_data('fftw_out.nc','realc', real(fldc(1,:,:)))
+            !call write_data('fftw_out.nc','imagc', dimag(fldc(1,:,:)))
+            !call write_data('fftw_out.nc','magc', abs(fldc(1,:,:)))
+            print *, '' 
+            print *, '2dc2c'
+            do i = 0, nlonb2/2
+                wlbN = exp(mui*2.*pi*real(i)/nlon)
+                a = wlbN - w1b2
+                b = cone - wlbN
+                c = cone - w1b2
+                d = cone - w1b2*wlbN
+                e = w1b2*b
+                cpout(1) = (a*fldc(1,1,i+1) + b*fldc(1,2,i+1))/c
+                cpout(2) = (d*fldc(1,2,i+1) - e*fldc(1,1,i+1))/c
+                print *, i, cpout(1:2)
+            enddo
+            print *, '2dc2c'
+            print *, ''
+            print *, ''
+        endif
+
+        if (mpp_npes()==1) then
+           ! clck_drcft = mpp_clock_id('drcft')
+           ! INIT=1
+           ! CALL DCRFT(INIT, fld1d, 1, fld1dout, 1, nlon, 1, 1, 1./nlon, & 
+           !             AUX1CRS, 22000, AUX1CRS(22001), 20000)
+
+           ! call mpp_clock_begin(clck_drcft)
+           ! INIT=0
+           ! CALL DCRFT(INIT, fld1d, 1, fld1dout, 1, nlon, 1, 1, 1./nlon, & 
+           !             AUX1CRS, 22000, AUX1CRS(22001), 20000)
+           ! call mpp_clock_end(clck_drcft)
+           ! print *, 'write data dcrft_out'
+           ! call write_data('dcrft_out.nc','fld', fld1dout(:))
+                
+            !call fft_1dr2c_serial(fld1d*scl, fldc1d) 
+            call fft_1dc2c_serial(fldc1d(:)) 
+            call write_data('fftw_out.nc','real', real(fldc1d(:)))
+            call write_data('fftw_out.nc','imag', dimag(fldc1d(:)))
+            call write_data('fftw_out.nc','mag', abs(fldc1d(:)))
+
+            print *, '1dr2c'
+            do i = 0, nlon
+                cpout(1) = fldc1d(i+1)
+                print *, i, cpout(1)
+            enddo
+            print *, '1dr2c'
+            print *, ''
+            print *, ''
+            print *, ''
+            
+        endif
+
+        call fms_io_exit()
+        call mpp_exit()
+            
     end subroutine init_grid_to_fourier
 
 end module grid_to_fourier_mod
